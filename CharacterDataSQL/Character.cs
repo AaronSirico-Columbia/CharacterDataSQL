0e5a75aa4438d0cb3329bd6cf5c512f527c30e65
AaronSirico <aarronsirico@gmail.com>
2023-10-25 16:41:27 -0500

    Add project files.

---
+++ CharacterDataSQL/Character.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterDataSQL
+{
+    public class Character
+    {
+
+        public int ID { get; set; }
+
+        public string Character_Name { get; set; }
+
+        public string Type { get; set; }
+
+        public string Map_Location { get; set; }
+
+        //hasValue
+        //.value
+        public bool? OriginalCharacter { get; set; }
+
+        public bool? SwordFighter { get; set; }
+
+        public bool? MagicUser { get; set; }
+
+
+
+        public Character(int id, string char_Name, string type, string map, string OG_Char, string Sword, string Magic)
+        {
+            ID = id;
+            Character_Name = char_Name;
+            Type = type;
+            Map_Location = map;
+            OriginalCharacter = isNull(OG_Char);
+            SwordFighter = isNull(Sword);
+            MagicUser = isNull(Magic);
+        }
+
+
+
+        public bool isNull(string x)
+        {
+            bool? i;
+            bool result;
+            if (!bool.TryParse(x, out result))
+            {
+                result = false; 
+                i = result;
+                i = null;            
+            }
+            else
+                return bool.Parse(x);
+            return result;
+           
+        }
+
+    }
+}
---
+++ CharacterDataSQL/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace CharacterDataSQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var Path = ($"{Directory.GetCurrentDirectory()}\\Files");
+            string File = $"{Path}\\Chars.csv";
+            List<Character> Characters = new List<Character>();
+            int i = 0;
+
+
+            using (StreamReader sr = new StreamReader(File))
+            {
+                var line = sr.ReadLine();
+                while (line != null)
+                {
+                    if (i < 1)
+                    {
+                        i++;
+                        line = sr.ReadLine();
+                    }
+                    else
+                    {
+                        var data = line.Split(',').ToList();
+                        Character character = new Character(1, data[0], data[1], data[2], data[3], data[4], data[5]);
+                        Characters.Add(character);
+                        line = sr.ReadLine();
+                    }
+
+                }
+            }
+
+
+
+
+        }
+    }
+}
